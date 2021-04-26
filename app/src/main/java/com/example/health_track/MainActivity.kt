43e238eace9f49c3fd51bb8ee94055cc7b4fadd2
Foxeeek <43350990+Foxeeek@users.noreply.github.com>
2021-04-26 13:50:35 +0300

    2 LR

--- app/src/main/java/com/example/health_track/MainActivity.kt
+++ app/src/main/java/com/example/health_track/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.health_track
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -23,8 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         aktivs = arrayListOf(
                 Aktiv("Пробежка",-250.00),
-                Aktiv("Пица",250.00),
-                Aktiv("Курица",250.00),
+                Aktiv("Курица",300.00),
+                Aktiv("Курица",300.00),
+                Aktiv("Курица",300.00),
+                Aktiv("Курица",300.00),
+                Aktiv("Турник",-500.00),
+                Aktiv("Турник",-500.00),
+                Aktiv("Турник",-500.00),
                 Aktiv("Турник",-500.00),
                 Aktiv("Пробежка",-250.00),
                 Aktiv("Хотьба",-50.00)
@@ -39,17 +45,28 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-//        updateDashboard()
+        updateDashboard()
 
     }
 
-//    @SuppressLint("WrongViewCast")
-//    private fun updateDashboard(){
-//        val totalVeight = aktivs.map { it.amount }.sum()
-//        val lostKkal = aktivs.filter { it.amount < 0 }.map { it.amount }.sum()
-//        val Kkallost = findViewById<TextView>(R.id.kkal_free)
-//
-//        Kkallost.text = "%2.f Ккал".format(lostKkal)
-//    }
+    private fun updateDashboard(){
+        val totalVeight = aktivs.map { it.amount }.sum()
+//        val lostKkal = aktivs.filter { it.amount < 0}.map { it.amount }.sum()
+//        val takeKkal = aktivs.filter { it.amount > 0}.map { it.amount }.sum()
+        val Kkallost :TextView = findViewById(R.id.kkal_free) as TextView
+        val KkallostText :TextView = findViewById(R.id.kkal_counter) as TextView
+
+//        val totalKkal = lostKkal + takeKkal
+        if (totalVeight < 0){
+            Kkallost.setTextColor(Color.GREEN)
+            KkallostText.text = "Потрачено калорий"
+        }else{
+            Kkallost.setTextColor(Color.RED)
+            KkallostText.text = "Набрано калорий"
+        }
+//        Kkallost.text = "%.2f Ккал".format(totalVeight)
+        Kkallost.text = "%.2f Ккал".format(Math.abs(totalVeight  ))
+
+    }
 
 }
