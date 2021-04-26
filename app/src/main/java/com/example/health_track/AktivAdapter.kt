6c33e084ba28a2d9cff0ef2828a564a9268ce9fb
Foxeeek <43350990+Foxeeek@users.noreply.github.com>
2021-04-26 03:54:05 +0300

    2 LR

---
+++ app/src/main/java/com/example/health_track/AktivAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.health_track
+
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class AktivAdapter(private  val Aktiv: ArrayList<Aktiv> ):
+        RecyclerView.Adapter<AktivAdapter.AktivHolder>() {
+
+    class AktivHolder(view: View): RecyclerView.ViewHolder(view){
+        val gymnastic : TextView = view.findViewById(R.id.gymnastic)
+        val amount : TextView = view.findViewById(R.id.amount)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AktivHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activ_layout,parent,false)
+        return AktivHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AktivHolder, position: Int) {
+        val aktivity = Aktiv[position]
+        val context = holder.amount.context
+
+        if (aktivity.amount < 0){
+
+            holder.amount.text = "- %.2f Ккал".format(Math.abs(aktivity.amount))
+            holder.amount.setTextColor(ContextCompat.getColor(context,R.color.green))
+        }else{
+            holder.amount.text = "+ %.2f Ккал".format(aktivity.amount)
+            holder.amount.setTextColor(ContextCompat.getColor(context,R.color.red))
+        }
+
+        holder.gymnastic.text = aktivity.label
+    }
+
+    override fun getItemCount(): Int {
+        return Aktiv.size
+    }
+
+}
--- app/src/main/java/com/example/health_track/MainActivity.kt
+++ app/src/main/java/com/example/health_track/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.health_track
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var aktivs : ArrayList<Aktiv>
+    private lateinit var aktivsAdapter: AktivAdapter
+    private lateinit var linearlayoutManager: LinearLayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rec_activ)
+
+
+        aktivs = arrayListOf(
+                Aktiv("Пробежка",-250.00),
+                Aktiv("Пица",250.00),
+                Aktiv("Курица",250.00),
+                Aktiv("Турник",-500.00),
+                Aktiv("Пробежка",-250.00),
+                Aktiv("Хотьба",-50.00)
+        )
+
+        aktivsAdapter = AktivAdapter(aktivs)
+        linearlayoutManager = LinearLayoutManager(this)
+
+        recyclerView.apply {
+            adapter = aktivsAdapter
+            layoutManager = linearlayoutManager
+
+        }
+
+//        updateDashboard()
+
     }
+
+//    @SuppressLint("WrongViewCast")
+//    private fun updateDashboard(){
+//        val totalVeight = aktivs.map { it.amount }.sum()
+//        val lostKkal = aktivs.filter { it.amount < 0 }.map { it.amount }.sum()
+//        val Kkallost = findViewById<TextView>(R.id.kkal_free)
+//
+//        Kkallost.text = "%2.f Ккал".format(lostKkal)
+//    }
+
 }
